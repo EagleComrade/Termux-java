0ed349132e31d85b8fed8936e387461413b0f4e0
LIJO VIJIN <83199761+EagleComrade@users.noreply.github.com>
2022-10-03 21:51:52 +0530

    Create install.sh
    It will install the java language for you. it help you accurately install the java with out any  errors

---
+++ install.sh
@@ -0,0 +1,45 @@
+
+
+logo() {
+       echo -e "..INSTALL JAVA ==> TERMUX"
+       echo -e ":::::::github@EagleComrade"
+        }
+install() {
+           echo -e "[+] INSTALLING CLANG..."
+           sleep 2s
+           pkg install clang -y
+           echo
+           echo
+           echo
+           echo -e "[+] INSTALLING JAVA(openjdk-17)"
+           sleep 2s
+           pkg install openjdk-17 -y
+           echo -e "............./done"
+           echo
+           echo
+           }
+#Main script
+clear 
+logo()
+sleep 2s
+echo -e "[/] UPDATEING & UPGRADING......"
+sleep 2s
+pkg update && pkg upgrade -y
+clear
+logo()
+echo
+echo -e "[\] STARTING MAIN INSTALLATIONs "
+sleep 2s
+install()
+echo
+echo
+FILE = "/data/data/com.termux/file/usr/bin"
+if [ -d "$FILE/java" ]:
+then
+echo -e "[\] INSTALLING SUCCESSFUL..."
+echo -e "[\] TYPE JAVA TO EXECUTE (~$ java)"
+else :
+echo -e "[\] INSTALLING FAILED "
+echo -e "(try to install one more time....)"
+fi
+echo
